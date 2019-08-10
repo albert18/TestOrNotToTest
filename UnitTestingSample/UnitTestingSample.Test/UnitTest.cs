6ad4b50f3d655fb734ef02f86b203e3a0da18b1b
albert18 <caps18@hotmail.com>
2019-08-10 12:23:11 -0400

    update

---
+++ UnitTestingSample/UnitTestingSample.Test/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestingSample.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        [TestCategory("Test Or Not To Test")]
+        [Owner("John Doe")]
+        public void DinnerTime_Return()
+        {
+            //Arrage
+            int currentResult = 1;
+
+            //Act
+            DinnerTime aa = new DinnerTime();
+            var expectedResult = aa.TimeToEat();
+
+            //Assert
+            Assert.IsTrue(expectedResult.Length > currentResult);
+        }
+    }
+}
