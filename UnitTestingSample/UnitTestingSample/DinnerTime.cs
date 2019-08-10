bb3dbe806e0f8d9d24298fd166f35399fb3577f9
albert18 <caps18@hotmail.com>
2019-08-10 11:06:18 -0400

    update

---
+++ UnitTestingSample/UnitTestingSample/DinnerTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingSample
+{
+    public class DinnerTime
+    {
+        public string TimeToEat()
+        {
+            var dinner = "Dinner Time Starts at 7:00pm tonight!";
+
+            return dinner;
+        }
+    }
+}
